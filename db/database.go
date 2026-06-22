23706058bdec751e8da7e712d12d459bcbbd51a0
Jxt-Eli <eaayinomba@st.knust.edu.gh>
2026-06-22 03:11:36 +0000

    fix: added ctx to database connection
    fix: Removed env vars from code and resolved other deliberate but stupid decisions made earlier

--- db/database.go
+++ db/database.go
@@ -1,23 +1,33 @@
 package db
 
-import(
-	// "fmt"
+import (
+	"context"
+	"fmt"
 	"log"
+	"os"
+	"time"
+
 	"database/sql"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/joho/godotenv"
 )
 
-func Connect() *sql.DB{
-	connstr := "host=/run/postgresql dbname=webdev port=5432 user=jxt-eli password=password1937 sslmode=disable"
-	
+func Connect() *sql.DB {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("local env not found. Defaulting to prod env vars")
+	}
+	connstr := os.Getenv("CONN_STR")
+
 	db, err := sql.Open("pgx", connstr)
-	if err != nil{
+	if err != nil {
 		log.Fatal("error loading credentials")
 	}
 
-	err = db.Ping()
-	if err != nil{
+	if err = db.PingContext(ctx); err != nil {
 		log.Fatal("failed to establish database connection")
 	}
 	// fmt.Printf("\ndb struct ptr: %v\n", db)
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.26.2
 require (
 	github.com/gorilla/mux v1.8.1
 	github.com/jackc/pgx/v5 v5.9.2
+	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.52.0
 )
 
